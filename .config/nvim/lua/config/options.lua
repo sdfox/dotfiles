a8acab99e39c1b6bdede59b15774539a8536d19c
sdfox <sdfox05@gmail.com>
2025-10-16 11:34:44 +0800

    new file:   fish/config.fish new file:   fish/fish_variables new file:   neovide/config.toml new file:   nvim/.gitignore new file:   nvim/.neoconf.json new file:   nvim/LICENSE new file:   nvim/README.md new file:   nvim/init.lua new file:   nvim/lazy-lock.json new file:   nvim/lazyvim.json new file:   nvim/lua/config/autocmds.lua new file:   nvim/lua/config/keymaps.lua new file:   nvim/lua/config/lazy.lua new file:   nvim/lua/config/options.lua new file:   nvim/lua/plugins/example.lua new file:   nvim/stylua.toml new file:   nviminit.vim new file:   tmux/tmux.conf new file:   ../.gitignore

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.o.cmdheight = 0  -- 隐藏命令行，减少底部空隙
+vim.o.laststatus = 3 -- 单状态栏
+vim.cmd('set lines=999 columns=999')  -- 强制最大化
