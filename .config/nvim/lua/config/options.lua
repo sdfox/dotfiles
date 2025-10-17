2bdc7f37d607c479f15e9540f494e3f3498f6d67
sdfox <sdfox05@gmail.com>
2025-10-17 23:39:22 +0800

    modified:   .config/nvim/lua/config/options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.cmdheight = 0  -- 隐藏命令行，减少底部空隙
-vim.o.laststatus = 3 -- 单状态栏
-vim.cmd('set lines=999 columns=999')  -- 强制最大化
+vim.opt.relativenumber = false
